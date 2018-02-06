b6e5a2d5d1f92692f444dd43c0d6f9ee10058da2
Martin Malchow <martin.malchow@hpi.de>
2018-02-06 15:11:03 +0100

    New database layout

--- db/install.sql
+++ db/install.sql
@@ -9,27 +9,100 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `fmslog` (
+--
+-- Datenbank: `nginxhlslog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `connections`
+--
+
+CREATE TABLE `connections` (
+  `id` int(11) NOT NULL,
+  `c-client-id` varchar(100) NOT NULL,
+  `c-ip` varchar(30) NOT NULL,
+  `c-agent` varchar(100) NOT NULL,
+  `c-ip-country` varchar(30) NOT NULL,
+  `streamname` varchar(100) NOT NULL,
+  `timestamp-start` datetime NOT NULL,
+  `timestamp-end` datetime NOT NULL,
+  `bytes` int(11) NOT NULL,
+  `duration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `last_update`
+--
+
+CREATE TABLE `last_update` (
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `last_update`
+--
+
+INSERT INTO `last_update` (`time`) VALUES
+('1970-01-01 12:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `log`
+--
+
+CREATE TABLE `log` (
   `id` int(11) NOT NULL,
   `c-client-id` varchar(100) DEFAULT NULL,
-  `c-ip` varchar(15) DEFAULT NULL,
+  `c-client-id-conn` varchar(100) NOT NULL,
+  `c-ip` varchar(30) DEFAULT NULL,
   `c-agent` varchar(100) NOT NULL,
   `c-ip-country` varchar(30) DEFAULT NULL,
-  `x-file-name` varchar(60) DEFAULT NULL,
-  `x-sname` varchar(40) DEFAULT NULL,
-  `connect-timestamp` datetime DEFAULT NULL,
-  `disconnect-timestamp` datetime DEFAULT NULL,
-  `x-duration` int(11) DEFAULT NULL,
-  `sc-bytes` int(11) DEFAULT NULL,
-  `sc-stream-bytes` int(11) DEFAULT NULL
+  `streamname` varchar(100) DEFAULT NULL,
+  `streamquality` varchar(100) DEFAULT NULL,
+  `connection-id` varchar(100) NOT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  `bytes` int(11) DEFAULT NULL,
+  `evaluated` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Indizes der exportierten Tabellen
+--
 
-ALTER TABLE `fmslog`
-  ADD PRIMARY KEY (`id`);
+--
+-- Indizes für die Tabelle `connections`
+--
+ALTER TABLE `connections`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `c-client-id` (`c-client-id`);
 
-ALTER TABLE `fmslog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Indizes für die Tabelle `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `timestamp` (`timestamp`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `connections`
+--
+ALTER TABLE `connections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT für Tabelle `log`
+--
+ALTER TABLE `log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3023;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
